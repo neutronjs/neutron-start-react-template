1762b4fd42d334aa25f0fd0903a32ea010753f29
Thiago de Souza <email@thiagodesouza.com.br>
2020-01-04 19:15:37 -0300

    chore: add notification styles and travis-ci configuration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import store from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import store from './store';
 import Routes from './routes';
 
 import GlobalStyled from './styles/global';
@@ -12,6 +14,12 @@ function App() {
     <Provider store={store}>
       <GlobalStyled />
       <Routes />
+      <ToastContainer
+        autoClose={8000}
+        closeButton={false}
+        pauseOnHover
+        position="bottom-right"
+      />
     </Provider>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Hello from '@/components/Hello';
+import Notification from '@/utils/notification';
 
 import { StyledContainer } from './styles';
 
 function Home() {
+  Notification.success('Hello, dev! How are you?');
   return (
     <StyledContainer>
       <Hello />
